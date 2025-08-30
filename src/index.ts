fda172791bdd9572c8d963e288d148545182e799
SantoshP2002 <santoshpawar96k@gmail.com>
2025-08-30 20:16:13 +0530

    Not Found Middleware DOne

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import express from "express";
+import "dotenv/config"
+import express, { Request, Response } from "express";
 import QueryString from "qs";
 
 import { PORT } from "./env";
-import { CorsMiddleware } from "./middlewares";
+import { CorsMiddleware, ResponseMiddleware } from "./middlewares";
 
 const app = express();
 
@@ -13,6 +14,15 @@ app.use(express.urlencoded({ extended: true }));
 app.set("query parser", (str: string) => QueryString.parse(str));
 
 app.use(CorsMiddleware.checkOrigin);
+
+app.get("/", (_: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to Bellavita API",
+  });
+})
+// Error Handling Routes
+app.use(ResponseMiddleware.notFound)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);

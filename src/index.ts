7707ba8b7a63d58dd7b92e24cab1c6f646940e43
SantoshP2002 <santoshpawar96k@gmail.com>
2025-08-30 19:28:10 +0530

    AppSuccess and AppError Class Constructor added

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import QueryString from "qs";
+
 import { PORT } from "./env";
 
 const app = express();
 
 const port = PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("query parser", (str: string) => QueryString.parse(str));
+
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });

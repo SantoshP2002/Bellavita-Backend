9a295924fcb9625a49d2525660ddbae57317f9bd
SantoshP2002 <santoshpawar96k@gmail.com>
2025-08-31 19:46:58 +0530

    Added Zod Middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,7 +1,7 @@
 // export * as AuthMiddleware from "./auth";
 export * as CorsMiddleware from "./cors";
 export * as DatabaseMiddleware from "./database";
-// export * as ZodMiddleware from "./zod";
+export * as ZodMiddleware from "./zod";
 // export * as MulterMiddleware from "./multer";
 export * as ResponseMiddleware from "./response";
 // export * as JSONParseMiddleware from "./JSONParse";

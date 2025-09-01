05ca291f68759bfa2dd0e67cc98f0c61e7cde15a
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-01 15:41:02 +0530

    register controller done with generate Token

--- src/modules/auth/index.ts
+++ src/modules/auth/index.ts
@@ -1,2 +1,4 @@
 export * as Controllers from "./controllers";
-export * as Routes from "./routes"
+export * as Routes from "./routes";
+export * as Validations from "./validations";
+export * as Services from "./services";

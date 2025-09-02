f045d5bde9b6f0add27c360d532614c1e401c39c
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-02 17:45:38 +0530

    Profile Pic Field added in User Model

--- src/modules/user/types/index.ts
+++ src/modules/user/types/index.ts
@@ -1,10 +1,11 @@
 export interface IUser {
-    _id: string;
-    email: string;
-    firstName: string;
-    lastName: string;
-    password: string;
-    role: "ADMIN" | "USER";
-    createdAt: Date;
-    updatedAt: Date;
+  _id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  profilePic: string;
+  role: "ADMIN" | "USER";
+  createdAt: Date;
+  updatedAt: Date;
 }

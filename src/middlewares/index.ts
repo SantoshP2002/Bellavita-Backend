9ef08b42320523b320b3c5a7edf19e26f7081081
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-02 18:11:05 +0530

    Removed Video and Other Type Media Multer handles

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,7 +2,7 @@
 export * as CorsMiddleware from "./cors";
 export * as DatabaseMiddleware from "./database";
 export * as ZodMiddleware from "./zod";
-// export * as MulterMiddleware from "./multer";
+export * as MulterMiddleware from "./multer";
 export * as ResponseMiddleware from "./response";
 // export * as JSONParseMiddleware from "./JSONParse";
 export * as RequestMiddleware from "./request";

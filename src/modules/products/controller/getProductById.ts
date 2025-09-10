7de2648906184d5a6e92f50c05f2caefed6c4e1c
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-10 17:27:17 +0530

    get product b ID controller

---
+++ src/modules/products/controller/getProductById.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import { Product } from "../models";
+import { AppError } from "../../../classes";
+
+export const getProductByIdController = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  if (!product) {
+    throw new AppError("Product not found", 404);
+  }
+  res.success(200, "Product found successfully", { product });
+};

34b379b8aa3db0347e3f41d735d4a8bbb1c8117a
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-17 10:09:41 +0530

    Refactor getAllProductsController to implement pagination and return total product count

--- src/modules/products/controller/getProducts.ts
+++ src/modules/products/controller/getProducts.ts
@@ -1,10 +1,27 @@
 import { Request, Response } from "express";
 import { Product } from "../models";
 
-export const getAllProductsController = async (
-  _req: Request,
-  res: Response
-) => {
-  const products = (await Product.find().lean()) ?? [];
-  res.success(200, "Products found successfully", { products });
+export const getAllProductsController = async (req: Request, res: Response) => {
+  const page = Number(req.query?.page);
+  const limit = Number(req.query?.limit);
+  const skip = page && limit ? (page - 1) * limit : 0;
+
+  // Pagination
+  let query = Product.find();
+
+  if (page && limit) {
+    query = query.skip(skip).limit(limit);
+  }
+
+  const [products, totalProducts] = await Promise.all([
+    query.lean().exec(),
+    Product.countDocuments().exec(),
+  ]);
+
+  res.success(200, "Products found successfully", {
+    products,
+    currentPage: page || 1,
+    totalProducts,
+    totalPages: page && limit ? Math.ceil(totalProducts / limit) : 1,
+  });
 };

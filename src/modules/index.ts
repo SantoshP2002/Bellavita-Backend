753f5849af77a242ae9dcbaea01599e007bf4483
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-21 19:03:35 +0530

    Add exports for CartModule and CartProductModule in the main module index

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,3 +2,4 @@ export * as UserModule from "./user";
 export * as AuthModule from "./auth";
 export * as ProductModule from "./products";
 export * as CartModule from "./cart";
+export * as CartProductModule from "./cartProduct";

d0ac56f8bea9412635505703e443da02d03018db
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-22 18:16:02 +0530

    Add export for Validations in cartProduct module

--- src/modules/cartProduct/index.ts
+++ src/modules/cartProduct/index.ts
@@ -2,3 +2,4 @@ export * as Controllers from "./controllers";
 export * as ITypes from "./types";
 export * as Routes from "./routes";
 export * as Models from "./model";
+export * as Validations from "./validation"

3965dcfd950eb73ab51f9ce6bac506bd9acc1983
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-22 18:14:57 +0530

    Add controller to update product quantity in cart

---
+++ src/modules/cartProduct/controllers/updateProductCartQuantity.ts
@@ -0,0 +1,25 @@
+import { Response } from "express";
+import { AuthorizedRequest } from "../../../types";
+import { CartProduct } from "../model";
+import { isValidMongoId } from "../../../utils";
+import { AppError } from "../../../classes";
+
+export const updateProductCartQuantityController = async (
+  req: AuthorizedRequest,
+  res: Response
+) => {
+  const { quantity } = req.body;
+
+  const { cartProductId } = req.params;
+  isValidMongoId(cartProductId, "Invalid cartProductId", 400);
+
+  const cartProduct = await CartProduct.findByIdAndUpdate(
+    cartProductId,
+    { quantity },
+    { new: true }
+  );
+
+  if (!cartProduct) throw new AppError("CartProduct not found", 404);
+
+  res.success(200, "Product quantity updated successfully", { quantity });
+};

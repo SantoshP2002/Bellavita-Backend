769db615dccde6c6298d697ceba1901dd8f28c89
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-23 18:36:51 +0530

    create removeProductToCartController

--- src/modules/cartProduct/routes/index.ts
+++ src/modules/cartProduct/routes/index.ts
@@ -8,6 +8,7 @@ import {
 import { addProductToCartController } from "../controllers";
 import { updateProductCartQuantityController } from "../controllers/updateProductCartQuantity";
 import { quantityZodSchema } from "../validation";
+import { removeProductToCartController } from "../controllers/removeProductToCart";
 
 export const router = Router();
 
@@ -23,4 +24,11 @@ router.patch(
   RequestMiddleware.checkEmptyRequest({ params: true }),
   ZodMiddleware.validateZodSchema(quantityZodSchema),
   ResponseMiddleware.catchAsync(updateProductCartQuantityController)
+);
+
+router.delete(
+  "/remove/:cartProductId",
+  AuthMiddleware.authenticated,
+  RequestMiddleware.checkEmptyRequest({ params: true }),
+  ResponseMiddleware.catchAsyncWithTransaction(removeProductToCartController)
 );

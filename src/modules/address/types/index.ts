dfe5457b4e3a3170e3395390106149d7f222d519
SantoshP2002 <santoshpawar96k@gmail.com>
2025-10-02 14:07:56 +0530

    create address types

---
+++ src/modules/address/types/index.ts
@@ -0,0 +1,22 @@
+import { Types } from "mongoose";
+import { UserModule } from "../..";
+import { ALLOW_COUNTRIES } from "../constants";
+
+export interface IAddress
+  extends Pick<UserModule.ITypes.IUser, "firstName" | "lastName" | "email"> {
+  user: Types.ObjectId;
+  phoneNumber: string;
+  altPhoneNumber: string;
+  address: string;
+  landmark: string;
+  city: string;
+  state: string;
+  pinCode: string;
+  country: (typeof ALLOW_COUNTRIES)[number];
+}
+
+export interface IUserAddress {
+  user: Types.ObjectId;
+  addresses: Types.ObjectId[];
+  defaultAddress: Types.ObjectId;
+}

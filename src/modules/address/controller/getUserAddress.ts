8905e3fcb753b9c051f748003cfa3561d6eac593
SantoshP2002 <santoshpawar96k@gmail.com>
2025-10-03 13:31:07 +0530

    remove unused lines in getUserAddressController

--- src/modules/address/controller/getUserAddress.ts
+++ src/modules/address/controller/getUserAddress.ts
@@ -7,12 +7,10 @@ export const getUserAddressController = async (
   res: Response
 ) => {
   const userId = req?.user?._id;
-  console.log("userId", userId);
 
   const userAddress = await UserAddress.findOne({ user: userId }).populate(
     "addresses"
   );
-  console.log("userAddress", userAddress);
 
   res.success(200, "User address fetched successfully", { userAddress });
 };

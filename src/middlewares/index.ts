ca9e4578fe0c19367ad4111aee79ed1dde74e4a6
SantoshP2002 <santoshpawar96k@gmail.com>
2025-10-22 13:20:08 +0530

    middleware file index.ts in added folder JSONParseMiddleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -4,5 +4,5 @@ export * as DatabaseMiddleware from "./database";
 export * as ZodMiddleware from "./zod";
 export * as MulterMiddleware from "./multer";
 export * as ResponseMiddleware from "./response";
-// export * as JSONParseMiddleware from "./JSONParse";
+export * as JSONParseMiddleware from "./JSONParse";
 export * as RequestMiddleware from "./request";

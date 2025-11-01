2396b49eda8ac42b9d7971f5d48f66265aae4a2e
SantoshP2002 <santoshpawar96k@gmail.com>
2025-11-01 14:42:19 +0530

    product module productSchema in added category and subCategory

--- src/modules/products/models/index.ts
+++ src/modules/products/models/index.ts
@@ -1,14 +1,37 @@
 import { model, Schema } from "mongoose";
 import { IProduct } from "../types";
 
-const productSchema = new Schema<IProduct>({
-  title: { type: String, required: true },
-  brand: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
-  sellingPrice: { type: Number, required: true },
-  images: { type: [String], required: true, default: [] },
-  category: { type: String, required: true },
-});
+const categorySchema = new Schema(
+  {
+    name: { type: String, required: true },
+    value: { type: String, required: true },
+  },
+  { versionKey: false, _id: false }
+);
+const subCategorySchema = new Schema(
+  {
+    name: { type: String, default: "" },
+    value: { type: String, default: "" },
+  },
+  { versionKey: false, _id: false }
+);
+
+const productSchema = new Schema<IProduct>(
+  {
+    title: { type: String, required: true },
+    brand: { type: String, required: true },
+    description: { type: String, required: true },
+    keyBenefits: { type: String, default: "" },
+    howToUse: { type: String, default: "" },
+    ingredients: { type: String, default: "" },
+    otherInformation: { type: String, default: "" },
+    price: { type: Number, required: true },
+    reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
+    sellingPrice: { type: Number, required: true },
+    images: { type: [String], required: true, default: [] },
+    category: categorySchema,
+    subCategory: subCategorySchema,
+  },
+  { versionKey: false, timestamps: true }
+);
 export const Product = model<IProduct>("Product", productSchema);

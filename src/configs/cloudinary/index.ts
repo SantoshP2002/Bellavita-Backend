726a32fe31b03017c353eaf8f50924c1870736ab
SantoshP2002 <santoshpawar96k@gmail.com>
2025-11-01 12:19:11 +0530

    configs\cloudinary in add CloudinaryConfigOption types and myCloudinary function in add image of CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET alo same product AND cloudinaryConnection in add CloudinaryConfigOption types

--- src/configs/cloudinary/index.ts
+++ src/configs/cloudinary/index.ts
@@ -1,23 +1,38 @@
 import { v2 as cloudinary } from "cloudinary";
+
+import { CloudinaryConfigOption } from "../../types";
 import {
   CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET,
   CLOUDINARY_CLOUD_NAME,
 } from "../../env";
 
-export const myCloudinary = () => {
-  cloudinary.config({
-    cloud_name: String(CLOUDINARY_CLOUD_NAME),
-    api_key: String(CLOUDINARY_API_KEY),
-    api_secret: String(CLOUDINARY_API_SECRET),
-    secure: true,
-  });
+export const myCloudinary = (
+  isImageOrVideoOrProduct: CloudinaryConfigOption
+) => {
+  if (isImageOrVideoOrProduct === "image") {
+    cloudinary.config({
+      cloud_name: CLOUDINARY_CLOUD_NAME as string,
+      api_key: CLOUDINARY_API_KEY as string,
+      api_secret: CLOUDINARY_API_SECRET as string,
+      secure: true,
+    });
+  } else if (isImageOrVideoOrProduct === "product") {
+    cloudinary.config({
+      cloud_name: CLOUDINARY_CLOUD_NAME as string,
+      api_key: CLOUDINARY_API_KEY as string,
+      api_secret: CLOUDINARY_API_SECRET as string,
+      secure: true,
+    });
+  }
   return cloudinary;
 };
 
-export const cloudinaryConnection = async () => {
+export const cloudinaryConnection = async (
+  isImageOrVideoOrProduct: CloudinaryConfigOption
+) => {
   try {
-    const cloudinary = myCloudinary();
+    const cloudinary = myCloudinary(isImageOrVideoOrProduct);
 
     const res = await cloudinary.api.ping();
     console.log(`Cloudinary Connected ✅`, res);

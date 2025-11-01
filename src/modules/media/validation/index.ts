e48be5ad096f8f292f2c30fcce82402ea77a3a9e
SantoshP2002 <santoshpawar96k@gmail.com>
2025-11-01 17:22:33 +0530

    media module validation in removeSingleImageZodSchema added

--- src/modules/media/validation/index.ts
+++ src/modules/media/validation/index.ts
@@ -1,9 +1,15 @@
 import z from "zod";
-import { validateZodString } from "../../../utils";
+import { validateZodString, validateZodUrl } from "../../../utils";
 
+// single and multiple image upload zod Schema
 export const uploadImageZodSchema = z.object({
   folderName: validateZodString({
     field: "folderName",
     blockSingleSpace: true,
   }),
+});
+
+// single image remove zod schema
+export const removeSingleImageZodSchema = z.object({
+  cloudUrl: validateZodUrl({ field: "cloudUrl" }),
 });

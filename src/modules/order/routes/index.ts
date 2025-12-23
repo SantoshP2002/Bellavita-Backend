93c693e0b5874f56a8a4092f63e666a3ee9b28e0
SantoshP2002 <santoshpawar96k@gmail.com>
2025-12-23 17:31:23 +0530

    add admin order retrieval route and reorganize order routes

--- src/modules/order/routes/index.ts
+++ src/modules/order/routes/index.ts
@@ -9,6 +9,7 @@ import { verifyPaymentController } from "../controllers";
 import { getOrderController } from "../controllers/getOrder";
 import { getByIdOrderController } from "../controllers/getIdOrder";
 import { cancelPaymentController } from "../controllers/cancelPayment";
+import { getAdminOrderController } from "../controllers/getAdminOrder";
 
 export const router = Router();
 
@@ -20,6 +21,7 @@ router.post(
   ResponseMiddleware.catchAsync(createOrderController)
 );
 
+// update order in verify-payment
 router.patch(
   "/verify-payment",
   RequestMiddleware.checkEmptyRequest({ body: true }),
@@ -27,21 +29,32 @@ router.patch(
   ResponseMiddleware.catchAsync(verifyPaymentController)
 );
 
+// get order user
 router.get(
   "/",
   AuthMiddleware.authenticated,
   ResponseMiddleware.catchAsync(getOrderController)
 );
-router.get(
-  "/:orderId",
-  RequestMiddleware.checkEmptyRequest({ params: true }),
-  AuthMiddleware.authenticated,
-  ResponseMiddleware.catchAsync(getByIdOrderController)
-);
 
+// update order user by ID
 router.patch(
   "/cancel-payment/:orderId",
   RequestMiddleware.checkEmptyRequest({ params: true }),
   AuthMiddleware.authenticated,
   ResponseMiddleware.catchAsync(cancelPaymentController)
+);
+
+// get Admin Orders
+router.get(
+  "/admin-order",
+  AuthMiddleware.authorized(["ADMIN"]),
+  ResponseMiddleware.catchAsync(getAdminOrderController)
+);
+
+// get order user by ID
+router.get(
+  "/:orderId",
+  RequestMiddleware.checkEmptyRequest({ params: true }),
+  AuthMiddleware.authenticated,
+  ResponseMiddleware.catchAsync(getByIdOrderController)
 );

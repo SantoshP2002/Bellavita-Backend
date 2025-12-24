01aa97adaaddeb00b5c9df30a58ba1ec20a0ef58
SantoshP2002 <santoshpawar96k@gmail.com>
2025-12-24 20:58:24 +0530

    blog route in add ( router )  get blog and get blog id

--- src/modules/blog/routes/index.ts
+++ src/modules/blog/routes/index.ts
@@ -1,19 +1,30 @@
 import { Router } from "express";
 import {
-  // AuthMiddleware,
+  AuthMiddleware,
   MulterMiddleware,
   ResponseMiddleware,
 } from "../../../middlewares";
 import { uploadBlogController } from "../controller/uploadBlog";
+import {
+  getBlogByIdController,
+  getBlogController,
+} from "../controller/getBlogController";
 
 export const router = Router();
 
+// Create Blog
 router.post(
   "/create",
-  // AuthMiddleware.authorized(["ADMIN"]),
+  AuthMiddleware.authorized(["ADMIN"]),
   MulterMiddleware.validateFiles({
     type: "single",
-    fieldName: "image"
+    fieldName: "image",
   }),
   ResponseMiddleware.catchAsync(uploadBlogController)
 );
+
+// Get Blog
+router.get("/", ResponseMiddleware.catchAsync(getBlogController));
+
+// Get Blog by ID
+router.get("/:id", ResponseMiddleware.catchAsync(getBlogByIdController));

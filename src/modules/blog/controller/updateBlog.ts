2964f50a5e7f247b330372fd811b8a51cc06d3ff
SantoshP2002 <santoshpawar96k@gmail.com>
2025-12-27 18:06:09 +0530

    updateBlog controller in add removeFromCloudinary  image functionality

--- src/modules/blog/controller/updateBlog.ts
+++ src/modules/blog/controller/updateBlog.ts
@@ -1,28 +1,37 @@
 import { Request, Response } from "express";
 import { Blog } from "../models";
 import { AppError } from "../../../classes";
-import { singleImageUploader } from "../../../utils";
+import { removeFromCloudinary, singleImageUploader } from "../../../utils";
 
 export const updateBlogController = async (req: Request, res: Response) => {
   const { id } = req.params;
   const update = req.body;
 
-  console.log("req.file", req.file);
+  const oldBlog = await Blog.findById(id);
+  if (!oldBlog) {
+    throw new AppError("Old Blog not found", 404);
+  }
 
   if (req.file) {
+    // delete old image
+    if (oldBlog.imagePublicId) {
+      await removeFromCloudinary(oldBlog.imagePublicId);
+    }
+
     const cldResp = await singleImageUploader({
       file: req.file,
-      folder:"Blog_Image"
-    })
+      folder: "Blog_Image",
+    });
 
     update.image = cldResp.secure_url;
+    update.imagePublicId = cldResp.public_id;
+
+    const blog = await Blog.findByIdAndUpdate(id, update, { new: true });
+
+    if (!blog) {
+      throw new AppError("Product not found in Update blog", 404);
+    }
+
+    res.success(200, "Blog Update Successfully", { blog });
   }
-
-  const blog = await Blog.findByIdAndUpdate(id, update, { new: true });
-
-  if (!blog) {
-    throw new AppError("Product not found in Update blog", 404);
-  }
-
-  res.success(200, "Blog Update Successfully", { blog });
 };

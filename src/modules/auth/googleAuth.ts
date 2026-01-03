eb4b77cd4378d5a97bc8bc8590e19dc72ad9a1ed
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-04 00:27:54 +0530

    redirection path updated

--- src/modules/auth/googleAuth.ts
+++ src/modules/auth/googleAuth.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { googleAuthClient } from "../../configs/o-auth";
 import { User } from "../user/models";
 import { generateToken } from "./services";
+import { FRONTEND_DEVELOPMENT_URL } from "../../env";
 
 // GOOGLE REDIRECT
 export const googleRedirect = async (_req: Request, res: Response) => {
@@ -12,14 +13,11 @@ export const googleCallback = async (req: Request, res: Response) => {
   try {
     const code = req.query.code as string; // Google se user ka data nikalta hai
     if (!code) {
-        console.error("Google callback missing code", req.query);
-        return res.redirect("http://localhost:5173/google-auth");
+      console.error("Google callback missing code", req.query);
+      return res.redirect(`${FRONTEND_DEVELOPMENT_URL}/google-auth}`);
     }
 
-    console.log("CODEIUM", code);
-
     const googleUser = await googleAuthClient.decode(code); // google api call OR User ka data milta hai
-    console.log("GOOGLEUSER", googleUser);
 
     const [firstName, ...rest] = googleUser.name.split(" ");
     const lastName = rest.join(" ") || "";
@@ -38,9 +36,9 @@ export const googleCallback = async (req: Request, res: Response) => {
     }
 
     const token = generateToken(user._id);
-    res.redirect(`http://localhost:5173/google-auth?token=${token}`);
+    res.redirect(`${FRONTEND_DEVELOPMENT_URL}/google-auth?token=${token}}`);
   } catch (error) {
     console.error("Google callback error:", error);
-    res.redirect("http://localhost:5173/google-auth");
+    res.redirect(`${FRONTEND_DEVELOPMENT_URL}/google-auth}`);
   }
 };

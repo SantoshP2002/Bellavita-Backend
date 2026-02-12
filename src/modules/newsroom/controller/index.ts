5685a9af75f661bd2863294d974666d2c7ec39c0
SantoshP2002 <santoshpawar96k@gmail.com>
2026-02-12 19:56:08 +0530

    newsroom controller index file update

--- src/modules/newsroom/controller/index.ts
+++ src/modules/newsroom/controller/index.ts
@@ -1,3 +1,4 @@
-export * from "./uploadNewsroom"
-export * from "./getNewsroom"
-export * from "./updateNewsroom"
+export * from "./uploadNewsroom";
+export * from "./getNewsroom";
+export * from "./updateNewsroom";
+export * from "./deleteNewsroom";
---
+++ src/modules/newsroom/index.ts
@@ -0,0 +1,4 @@
+export * as NTypes from "./types";
+export * as Models from "./models";
+export * as controllers from "./controller";
+export * as Routes from "./routes";
